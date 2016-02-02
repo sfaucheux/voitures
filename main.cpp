413b72cdc9b1bf6568fbe8f2fe9dfc70ce996f0a
Tateyuki Shigaraki <phkbphkbphkb@gmail.com>
2016-02-02 14:56:20 +0100

    Jeu de voitures trop de ouf \!\!\!

--- main.cpp
+++ main.cpp
@@ -7,8 +7,7 @@ int main(int argc, char** argv)
 	Renderer renderer;
 	Context context(renderer);
 
-    printf("%d, %d\n", sizeof(glm::vec3), sizeof(float));
-    context.init(1024, 768, "voitures", 0);
+    context.init(1024, 768, "Jeu de voitures trop de ouf !!!", 0);
     while (context.eventLoop())
     {
         context.clean();
