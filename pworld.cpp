c016b23ad7e7317bddb5f6b7b0b0b0ffbf86b305
Twixx <faucheux.seb@gmail.com>
2016-02-02 08:45:03 +0100

    pworld

---
+++ pworld.cpp
@@ -0,0 +1,17 @@
+#include "pworld.h"
+
+using namespace glm ;
+
+PWorld::PWorld()
+{
+
+}
+void PWorld::setGravity(vec3 g)
+{
+    m_gravity = g;
+}
+void PWorld::addObject(PObject* object)
+{
+    m_objects.push_back(object);
+}
+
--- pworld.h
+++ pworld.h
@@ -1,22 +1,22 @@
-#ifdef PWorld_H
+#ifndef PWorld_H
 #define PWorld_H
 
 #include <list>
 #include "glm/glm.hpp"
+#include "pobject.h"
 
 class PWorld 
 {
     public:
         PWorld();
-        void setGravity(float gravity);
+        void setGravity(glm::vec3 gravity);
         void update();
-        void addObject(PObject&);
-        void removeObject(PObject&);
+        void addObject(PObject*);
+        void removeObject(PObject*);
 
     private:
-        std::list<PObject> m_objects ;
+        std::list<PObject*> m_objects ;
         glm::vec3 m_gravity ;
-
 
 };
 #endif
