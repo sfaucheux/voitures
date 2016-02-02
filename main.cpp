e48507803598d357c7e23b25a58a06df112e04f4
Tateyuki Shigaraki <phkbphkbphkb@gmail.com>
2016-02-02 14:53:11 +0100

    Fini le moteur graphique pour le moment

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include "context.h"
 #include "renderer.h"
+#include "glm/glm.hpp"
 
 int main(int argc, char** argv)
 {
 	Renderer renderer;
 	Context context(renderer);
 
+    printf("%d, %d\n", sizeof(glm::vec3), sizeof(float));
     context.init(1024, 768, "voitures", 0);
     while (context.eventLoop())
     {
