ba7447e56cd85b3774182411d35a178c738e121c
Tateyuki Shigaraki <phkbphkbphkb@gmail.com>
2016-02-02 08:30:59 +0100

    Initialisation du context

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char** argv)
 	Renderer renderer;
 	Context context(renderer);
 
+    context.init(1024, 768, "voitures", 0);
     while (context.eventLoop())
     {
         context.clean();
