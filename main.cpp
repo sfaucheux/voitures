c9bec37c1fca65ef1b8f3127b314849943a67fea
Twixx <faucheux.seb@gmail.com>
2016-02-03 09:03:46 +0100

    error message when context is not initialized.

--- main.cpp
+++ main.cpp
@@ -7,12 +7,17 @@
 #define width 1024
 #define height 768
 
+using namespace std ;
+
 int main(int argc, char** argv)
 {
     Renderer renderer;
 	Context context(renderer);
     if (!context.init(width, height, "Jeu de voitures trop de ouf !!!", 0))
-        exit(EXIT_FAILURE);
+    {
+        cerr << "Impossible d'initialiser le contexte OpenGL." << endl ;
+        return 0;
+    }
 
     PMesh* obj = new PMesh();
     obj->loadObject("cube.obj");
@@ -26,7 +31,6 @@ int main(int argc, char** argv)
     while (context.eventLoop())
     {
         context.clean();
-        drw.setModel(glm::mat4(1));
         renderer.draw(drw); 
         context.show();
     }
