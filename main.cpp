673267f52e104ba7c1120f6b6ac53210254d95b7
ch0c0pain <sylvain.gauthier@ensimag.grenoble-inp.fr>
2016-02-04 15:05:41 +0100

    corrigé le délai du sleep

--- main.cpp
+++ main.cpp
@@ -46,9 +46,9 @@ int main(int argc, char** argv)
         obj.setShader(&shdr1);
         renderer.draw(obj, GL_LINE); 
         context.show();
-        obj.rotate(glm::vec3(0,0.001,0));
+        obj.rotate(glm::vec3(0,0.01,0));
 
-        usleep(100);
+        usleep(10000);
     }
 
     return 0;
