3e167efd185b983768474e4e4bd4c5712659fe27
ch0c0pain <sylvain.gauthier@ensimag.grenoble-inp.fr>
2016-02-04 22:27:21 +0100

    ajouté la gestion des uvmap

--- objects/mesh.cpp
+++ objects/mesh.cpp
@@ -1,78 +1,161 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <map>
 
 #include "mesh.h"
 
 using namespace std;
 using namespace glm;
 
+
 Mesh::Mesh(string filename)
 {
 	cout << "Loading " << filename << endl;
-    ifstream inFile ("models/" + filename);
-    string line;
+	ifstream inFile ("models/" + filename);
+	string line;
 
-    if (inFile.is_open())
-    {
-        string ident;
-        vec3 point;
+	if (inFile.is_open())
+	{
+		string ident;
+		vec3 point;
+		vec2 UV;
 
-        while (getline(inFile, line))
-        {
-            stringstream ss (stringstream::in | stringstream::out);
-            ss << line;
-            
-            ss >> ident;
+		//Temporary vectors filled with vertices and uvs, they are not necessarily in the same order
 
-            if (ident == "v")
-            {
-                ss >> point.x >> point.y >> point.z;
-                m_vertices.push_back(point);
-            }
-            else if (ident == "f")
-            {
-                int c = 0;
-                int last;
-                int vert[4];
-                uvec3 face;
-                string cur = "plop";
-                string subcur;
+		while (getline(inFile, line))
+		{
+			stringstream ss (stringstream::in | stringstream::out);
+			ss << line;
 
-                while (cur != "" && c < 4)
-                {
-                    cur = "";
-                    ss >> cur;
-                    stringstream subss (stringstream::in | stringstream::out);
-                    subss << cur;
-                    getline(subss, subcur, '/');
-					if (subcur != "")
+			ss >> ident;
+
+			if (ident == "v")
+			{
+				ss >> point.x >> point.y >> point.z;
+				vertTmp.push_back(point);
+			}
+			else if (ident == "vt")
+			{
+				ss >> UV.x >> UV.y;
+				uvsTmp.push_back(UV);
+			}
+			else if (ident == "f")
+			{
+				int c = 0;
+				int fLineInd = 0;
+				int vert[4], uvs[4];
+				uvec3 face;
+				uvec3 uvFace;
+				string cur = "plop";
+				string subcur;
+
+				while (cur != "" && c < 4)
+				{
+					cur = "";
+					ss >> cur;
+					stringstream subss (stringstream::in | stringstream::out);
+					subss << cur;
+					fLineInd = 0;
+
+					while (getline(subss, subcur, '/'))
 					{
-                    	vert[c] = stoi(subcur) - 1;
-                    	c++;
+						if (subcur != "")
+						{
+							switch (fLineInd)
+							{
+								case 0:
+									vert[c] = stoi(subcur) - 1;
+									break;
+
+								case 1:
+									uvs[c] = stoi(subcur) - 1;
+									break;
+
+								case 2:
+									break;
+
+								default:
+									cout << "Warning, weird stuff in obj file" << endl;
+							}
+						}
+						fLineInd ++;
 					}
-                }
+					c++;
+				}
 
-                face.x = vert[0];
-                face.y = vert[1];
-                face.z = vert[2];
+				addCouple(vert[0],uvs[0]);
+				addCouple(vert[1],uvs[1]);
+				addCouple(vert[2],uvs[2]);
 
-                m_faces.push_back(face);
+				/*m_faces.push_back(vert[0]);
+				  m_faces.push_back(vert[1]);
+				  m_faces.push_back(vert[2]);
 
-                if(c == 4)
-                {
-                    face.y = face.z;
-                    face.z = vert[3];
-                    m_faces.push_back(face);
-                }
-            }
-        }
-        m_gObj.load(m_vertices, m_faces/*, m_textures*/); //TODO
-        m_pObj = new PObject(); // = new PMesh();
-        cout << "Loading successful" << endl;
-    }
-    else
-        cout << "Error while loading model, no file found" << endl;
+				  m_uvindices.push_back(uvs[0]);
+				  m_uvindices.push_back(uvs[1]);
+				  m_uvindices.push_back(uvs[2]);*/
+
+				if(c == 4)
+				{
+					addCouple(vert[0],uvs[0]);
+					addCouple(vert[2],uvs[2]);
+					addCouple(vert[3],uvs[3]);
+					/*m_faces.push_back(vert[0]);
+					  m_faces.push_back(vert[2]);
+					  m_faces.push_back(vert[3]);
+
+					  m_uvindices.push_back(uvs[0]);
+					  m_uvindices.push_back(uvs[2]);
+					  m_uvindices.push_back(uvs[3]);*/
+				}
+			}
+		}
+
+		m_gObj.load(m_vertices, m_indices, m_uvmap); //TODO
+		m_pObj = new PObject(); // = new PMesh();
+		cout << "Loading successful" << endl;
+	}
+	else
+		cout << "Error while loading model, no file found" << endl;
+
+	m_vertices.clear();
+	m_vertices.shrink_to_fit();
+
+	m_indices.clear();
+	m_indices.shrink_to_fit();
+
+	m_uvmap.clear();
+	m_uvmap.shrink_to_fit();
+
+	m_normals.clear();
+	m_normals.shrink_to_fit();
+}
+
+//Add a couple of (vertex, UV) and its index in the arrays. Avoid redundancy of data
+void Mesh::addCouple(unsigned int vertID, unsigned int uvID)
+{
+	map<uvec2,unsigned int>::iterator it;
+
+	//Has the couple (vert,uv) already been seen ?
+	it = knowns.find(uvec2(vertID, uvID));
+
+	if (it == knowns.end())
+	{
+		//If yes, all we have to do is add the index we stored in the indices array
+		m_indices.push_back(it->second);
+	}
+	else
+	{
+		//If no, we add a new couple of (vertex,uv) to the definitive array, a new index
+		//pointing to this couple and we declare the couple in the "knowns" map.
+
+		m_vertices.push_back(vertTmp.at(vertID));
+		m_uvmap.push_back(uvsTmp.at(uvID));
+		m_indices.push_back(m_vertices.size() - 1);
+
+		knowns[uvec2(vertID,uvID)] = m_vertices.size() - 1;
+	}
 }
 
 Mesh::~Mesh()
--- objects/mesh.h
+++ objects/mesh.h
@@ -19,8 +19,24 @@ class Mesh : public Object
 
 	private:
 		std::vector<glm::vec3> m_vertices;
-        std::vector<glm::uvec3> m_faces;
-        std::vector<int> m_normals;
+        std::vector<unsigned int> m_indices;
+        std::vector<glm::vec2> m_uvmap;
+        std::vector<unsigned int> m_normals;
+
+		//Super bof de déclarer ceux-là en membres de la classe, mais bon...
+
+		struct comp
+		{
+			bool operator()(const glm::uvec2& a, const glm::uvec2& b) const
+			{
+				return a.x < b.x;
+			}
+		};
+
+		void addCouple(unsigned int vertID, unsigned int uvID);
+		std::map<glm::uvec2,unsigned int,comp> knowns;
+		std::vector<glm::vec3> vertTmp;
+		std::vector<glm::vec2> uvsTmp;
 
 };
 
