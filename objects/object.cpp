b50475c2d8573d4a49d3c5839d060efaca8c76f5
Tateyuki Shigaraki <phkbphkbphkb@gmail.com>
2016-02-13 14:58:12 +0100

    Fixed wrong orientation drawn

--- objects/object.cpp
+++ objects/object.cpp
@@ -42,13 +42,11 @@ void Object::sync()
 void Object::translate(glm::vec3 t)
 {
     m_pObj->translate(t);
-    m_gObj.translate(t);
+    this->sync();
 }
 
 void Object::rotate(glm::vec3 r)
 {
     m_pObj->rotate(r);
-    m_gObj.rotate(glm::vec3(1, 0, 0), r.x);
-    m_gObj.rotate(glm::vec3(0, 1, 0), r.y);
-    m_gObj.rotate(glm::vec3(0, 0, 1), r.z);
+    this->sync();
 }
