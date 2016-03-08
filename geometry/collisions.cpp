b5f1e5f6097307a7bcc5898fe4eaa7b35095170f
Tateyuki Shigaraki <phkbphkbphkb@gmail.com>
2016-03-08 20:24:55 +0100

    Fixed box-sphere collision points/normals

--- geometry/collisions.cpp
+++ geometry/collisions.cpp
@@ -50,31 +50,27 @@ bool Collisions::collide(const Sphere* s, const Box* b)
             return zc - r <= b->getDepth() / 2.0;
         if (zc <= b->getDepth() / 2.0)
             return yc - r <= b->getHeight() / 2.0;
-        else if (yc <= b->getHeight() / 2.0 + r)
+        if (yc <= b->getHeight() / 2.0 + r)
             return square(yc - (b->getHeight() / 2.0)) + square(zc - (b->getDepth() / 2.0)) <= square(r);
-        else
-            return false;
+        return false;
     }
     else if (yc <= b->getHeight() / 2.0)
     {
         if (zc <= b->getDepth() / 2.0)
             return xc - r <= b->getWidth() / 2.0;
-        else if (zc <= b->getDepth() / 2.0 + r)
+        if (zc <= b->getDepth() / 2.0 + r)
             return square(xc - (b->getWidth() / 2.0)) + square(zc - (b->getDepth() / 2.0)) <= square(r);
-        else
-            return false;
+        return false;
     }
     else if (zc <= b->getDepth() / 2.0)
     {
         if (xc <= b->getWidth() / 2.0 + r)
             return square(xc - (b->getWidth() / 2.0)) + square(yc - (b->getHeight() / 2.0)) <= square(r);
-        else
-            return false;
+        return false;
     }
     else if ((xc <= b->getWidth() / 2.0 + r) && (yc <= b->getHeight() / 2.0 + r) && (zc <= b->getDepth() / 2.0 + r))
         return square(xc - (b->getWidth() / 2.0)) + square(yc - (b->getHeight() / 2.0)) + square(zc - (b->getDepth() / 2.0)) <= square(r);
-    else
-        return false;
+    return false;
 }
 
 bool Collisions::collide(const Box* b, const Sphere* s)
@@ -95,45 +91,58 @@ vector<tuple<vec3,vec3>> Collisions::collisionPoints(const Sphere* s, const Box*
     if (xc <= b->getWidth() / 2.0)
     {
         if (yc <= b->getHeight() / 2.0)
+        {
             if (zc - r <= b->getDepth() / 2.0)
                 collisionPoint = sphPos - sgn(sphPos.z) * glm::vec3(0, 0, r);
-        if (zc <= b->getDepth() / 2.0)
+        }
+        else if (zc <= b->getDepth() / 2.0)
+        {
             if (yc - r <= b->getHeight() / 2.0)
                 collisionPoint = sphPos - sgn(sphPos.y) * glm::vec3(0, r, 0);
+        }
         else if (yc <= b->getHeight() / 2.0 + r)
+        {
             if (square(yc - (b->getHeight() / 2.0)) + square(zc - (b->getDepth() / 2.0)) <= square(r))
-            {
-                cout << "zboub";
                 collisionPoint = glm::vec3(sphPos.x, sgn(sphPos.y) * b->getHeight() / 2.0, sgn(sphPos.z) * b->getDepth() / 2.0);
-            }
+        }
         else
             return vector<tuple<vec3,vec3>>();
     }
     else if (yc <= b->getHeight() / 2.0)
     {
         if (zc <= b->getDepth() / 2.0)
+        {
             if (xc - r <= b->getWidth() / 2.0)
                 collisionPoint = sphPos - sgn(sphPos.x) * glm::vec3(r, 0, 0);
+        }
         else if (zc <= b->getDepth() / 2.0 + r)
+        {
             if (square(xc - (b->getWidth() / 2.0)) + square(zc - (b->getDepth() / 2.0)) <= square(r))
                 collisionPoint = glm::vec3(sgn(sphPos.x) * b->getWidth() / 2.0, sphPos.y, sgn(sphPos.z) * b->getDepth() / 2.0);
+        }
         else
             return vector<tuple<vec3,vec3>>();
     }
     else if (zc <= b->getDepth() / 2.0)
     {
         if (xc <= b->getWidth() / 2.0 + r)
+        {
             if (square(xc - (b->getWidth() / 2.0)) + square(yc - (b->getHeight() / 2.0)) <= square(r))
                 collisionPoint = glm::vec3(sgn(sphPos.x) * b->getWidth() / 2.0, sgn(sphPos.y) * b->getHeight() / 2.0, sphPos.z);
+        }
         else
             return vector<tuple<vec3,vec3>>();
     }
     else if ((xc <= b->getWidth() / 2.0 + r) && (yc <= b->getHeight() / 2.0 + r) && (zc <= b->getDepth() / 2.0 + r))
+    {
         if (square(xc - (b->getWidth() / 2.0)) + square(yc - (b->getHeight() / 2.0)) + square(zc - (b->getDepth() / 2.0)) <= square(r))
             collisionPoint = glm::vec3(sgn(sphPos.x) * b->getWidth() / 2.0, sgn(sphPos.y) * b->getHeight() / 2.0, sgn(sphPos.z) * b->getDepth() / 2.0);
+    }
     else
+    {
         return vector<tuple<vec3,vec3>>();
-    vec3 n = /*mat3(b->getRotationMatrix()) * */(collisionPoint - sphPos);
+    }
+    vec3 n = mat3(b->getRotationMatrix()) * (collisionPoint - sphPos);
     cout << n.x << ";" << n.y << ";" << n.z << endl;
     return vector<tuple<vec3,vec3>>({make_tuple(b->getWorldPoint(collisionPoint), mat3(b->getRotationMatrix()) * (collisionPoint - sphPos))});
 }
